29f3b626ad77a41ebcd5d080e977e130ef4fcb2f
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-25 17:12:46 +0000

    Pass op with search term and rename func

--- pgp/get_pgp_key.py
+++ pgp/get_pgp_key.py
@@ -3,9 +3,12 @@ import requests
 import sys
 
 
-def get_pgp_key(search_term):
+def get_info_from_keyserver(search_term, op_value="index"):
 
-    url = "https://hkps.pool.sks-keyservers.net/pks/lookup?op=index&search={}".format(search_term)
+    # curl -s 'https://pgp.mit.edu/pks/lookup?op=get&search=0xsomefingerprint'
+
+    url = "https://hkps.pool.sks-keyservers.net/pks/lookup?op={op_value}&search={search_term}".format(
+        op_value=op_value, search_term=search_term)
     print(url)
     response = requests.get(url, verify='sks-keyservers.net-CA.pem')
     return response.text
@@ -13,11 +16,16 @@ def get_pgp_key(search_term):
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        sys.exit("\n\nUsage: {} search_term\n\n".format(sys.argv[0]))
-    search_term = sys.argv[1]
-    html_result = get_pgp_key(search_term)
+    arg_list = []
+    arg_count = len(sys.argv)
+    print(arg_count)
+
+    if (arg_count < 2) or (arg_count > 3):
+        sys.exit("\n\nUsage: {} search_term [op_value]\n\n".format(sys.argv[0]))
+
+    arg_list.append(sys.argv[1])
+    if arg_count > 2:
+        arg_list.append(sys.argv[2])
+
+    html_result = get_info_from_keyserver(*arg_list)
     pprint.pprint(html_result)
-
-
-
