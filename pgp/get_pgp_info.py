0df5f4b6d35e5aa6e4a08d7b3929aeaf76d6960f
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-28 16:53:15 +0000

    Support multiple servers.

--- pgp/get_pgp_info.py
+++ pgp/get_pgp_info.py
@@ -1,16 +1,22 @@
 import pprint
 import requests
 import sys
+from urllib.parse import urlparse
 
+SERVERS = ["hkps://hkps.pool.sks-keyservers.net"]
 
 # Example: python get_pgp_info.py fred@flintstone.com get
 
 def get_info_from_keyserver(search_term, op_value="index"):
 
-    url = "https://hkps.pool.sks-keyservers.net/pks/lookup"
-    payload = {'op':op_value, 'search':search_term, 'options':'mr'}
-    response = requests.get(url, params=payload, verify='sks-keyservers.net-CA.pem')
-    print(response.url)
+    for key_server in SERVERS:
+        parse_result = urlparse(key_server)
+        url = "https://{}/{}".format(parse_result.netloc, 'pks/lookup')
+        payload = {'op':op_value, 'search':search_term, 'options':'mr',\
+                'fingerprint':'on'}
+        response = requests.get(url, params=payload, verify='sks-keyservers.net-CA.pem')
+        if (search_term in response.text):
+            break
     return response.text
 
 
@@ -18,7 +24,6 @@ if __name__ == '__main__':
 
     arg_list = []
     arg_count = len(sys.argv)
-    print(arg_count)
 
     if (arg_count < 2) or (arg_count > 3):
         sys.exit("\n\nUsage: {} search_term [op_value]\n\n".format(sys.argv[0]))
