3a1b37b9848289846406f6f4495348b56b3343a3
Phil Ratcliffe <phil@philratcliffe.co.uk>
2022-11-01 07:43:14 +0000

    Removed unused imports

--- get_van.py
+++ get_van.py
@@ -1,7 +1,5 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 url = "https://markets.ft.com/data/funds/tearsheet/historical?s=GB00B3ZHN960:GBP"
