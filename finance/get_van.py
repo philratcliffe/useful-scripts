b6742e8339da6bd15d77a545d2da6f5dcab7d679
Phil Ratcliffe <phil@philratcliffe.co.uk>
2022-11-23 13:32:38 +0000

    Simplify output
    Reduce the number of columns, and make the date more readable.

--- finance/get_van.py
+++ finance/get_van.py
@@ -5,6 +5,12 @@ import pandas as pd
 url = "https://markets.ft.com/data/funds/tearsheet/historical?s=GB00B3ZHN960:GBP"
 
 df = pd.read_html(url, flavor="bs4")[0]
+
+# select columns we want
+df = df[["Date", "High"]]
+
+df['Date'] = df['Date'].str.split(',')
+df['Date'] = df['Date'].apply(lambda x: ','.join([x[0], x[1], x[4]]))
 print(df)
 
 
